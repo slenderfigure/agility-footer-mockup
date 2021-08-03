0c325c60c75bd5682a39e8dd01b407a26a411fee
Adison P <hirayoki.nova@gmail.com>
2021-08-03 10:37:03 -0400

    Added onscroll height and content style change to the navbar. Included content placeholders to create depth

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 'use strict mode';
 
+const onScroll = () => {
+  const navbar = document.querySelector('#main-navbar');
+
+  if (window.scrollY > 0) navbar.setAttribute('data-scrolled', '');
+  else navbar.removeAttribute('data-scrolled');
+}
+
+window.addEventListener('scroll', onScroll);
