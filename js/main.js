50afbefc484e9055f98200efa4c925e9d032bfae
Adison P <hirayoki.nova@gmail.com>
2021-08-24 09:18:45 -0400

    Modified several things

--- js/main.js
+++ js/main.js
@@ -4,6 +4,27 @@ const body = document.body;
 const header = body.querySelector('.header');
 const bodyWrapper = body.querySelector('.body-wrapper');
 const mobileMenu = body.querySelector('.mobile-menu');
+const linkList = document.querySelector('.header__main-links-list');
+
+const showDropdown = e => {
+  e.preventDefault();
+
+  if (!e.target.closest('[data-expandable-link]')) return; 
+
+  const link = e.target.closest('[data-expandable-link]');
+  const dropdown = link.querySelector('.dropdown');
+
+  if (dropdown.hasAttribute('active-dropdown')) closeDropdown();
+
+  dropdown.setAttribute('active-dropdown', ''); 
+
+  const closeDropdown = e => {
+    dropdown.removeAttribute('active-dropdown');
+    window.removeEventListener('click', closeDropdown);
+  }
+
+  setTimeout(() => window.addEventListener('click', closeDropdown), 100);
+}
 
 const closeMobileNavMenu = () => {
   [
@@ -48,3 +69,5 @@ document.querySelector('.header__menu-btn')
 
 document.querySelector('.mobile-menu__close-btn')
   .addEventListener('click', closeMobileNavMenu);
+
+linkList.addEventListener('click', showDropdown);
